e844e33b918e23cefd3ab098c0f64790d5c8b348
viewtronaayush <aayushhindalekar20@gmail.com>
2026-03-30 13:54:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def generate_mcq(text):
     """
 
     response = client.chat.completions.create(
-        model="llama3-70b-8192",
+        model="llama3-8b-8192",
         messages=[
             {"role": "user", "content": prompt}
         ],
